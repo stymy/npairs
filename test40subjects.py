25e9321d4b236c2daddb874b8335134ff1ae5003
stymy <rschadmin@cmi-rsch-li002.childmind.org>
2014-02-26 14:48:02 -0500

    datagrabber works for cent & eigen now

--- test40subjects.py
+++ test40subjects.py
@@ -10,38 +10,56 @@ import os
 from text_out import Text_out
 import numpy as np
 
-from variables import workingdir, datadir, subjects, derivs, preprocs
+from variables import workingdir, datadir, derivdir, subjects, derivs, preprocs, pprocs, methods, deriv_types
 
 def get_wf():
     wf = pe.Workflow(name="svc_workflow")
-    wf.base_dir = os.path.join(workingdir,"npairs")
+    wf.base_dir = os.path.join(workingdir,"allNpairs")
     wf.config['execution']['crashdump_dir'] = wf.base_dir + "/crash_files"
 
     #INFOSOURCE ITERABLES
     subject_id_infosource = pe.Node(util.IdentityInterface(fields=['subject_id']), name="subject_id_infosource")
     subject_id_infosource.iterables = [('subject_id', subjects)]
+
+    method_id_infosource = pe.Node(util.IdentityInterface(fields=['method_id']), name="method_id_infosource")
+    method_id_infosource.iterables = ('method_id', methods)
     
     deriv_id_infosource = pe.Node(util.IdentityInterface(fields=['deriv_id']), name="deriv_id_infosource")
     deriv_id_infosource.iterables = ('deriv_id', derivs)
 
     preproc_id_infosource = pe.Node(util.IdentityInterface(fields=['preproc_id']), name="preproc_id_infosource")
     preproc_id_infosource.iterables = ('preproc_id', preprocs)
+    
+    pproc_id_infosource = pe.Node(util.IdentityInterface(fields=['pproc_id']), name="pproc_id_infosource")
+    pproc_id_infosource.iterables = ('pproc_id', pprocs)
+    
+    deriv_type_infosource = pe.Node(util.IdentityInterface(fields=['deriv_type']), name="deriv_type_infosource")
+    deriv_type_infosource.iterables = ('deriv_type', deriv_types)
 
     #DATAGRABBER
-    datagrabber = pe.Node(nio.DataGrabber(infields=['subject_id','deriv_id','preproc_id'], outfields=derivs), name='datagrabber')
-    datagrabber.inputs.base_directory = datadir
-    datagrabber.inputs.template='%s*/%s/_scan_rest*/*/*/*/%s/*/*/*/*.nii.gz'
-    datagrabber.inputs.template_args['deriv_files'] = [['subject_id', 'deriv_id','preproc_id']]
+    datagrabber = pe.Node(nio.DataGrabber(infields=['subject_id','deriv_id','preproc_id',
+    'deriv_type'], outfields=['methods_files','centrality_files']), name='datagrabber')
+    datagrabber.inputs.base_directory = '/'
+    datagrabber.inputs.template = '*'
+    datagrabber.inputs.field_template = dict(methods_files=os.path.join(datadir,'%s*/%s/_scan_rest*/*/*/*/%s/*/*/*/*.nii.gz'),
+                                            centrality_files= os.path.join(derivdir,'%s/%s*/*zscore/*/%s/*%s*.nii.gz'))
+    datagrabber.inputs.template_args = dict(methods_files= [['subject_id', 'method_id','preproc_id']],
+                                            centrality_files= [['deriv_id','subject_id','pproc_id','deriv_type']])
     datagrabber.inputs.sort_filelist = True
 
     wf.connect(subject_id_infosource, 'subject_id', datagrabber, 'subject_id')
     wf.connect(deriv_id_infosource, 'deriv_id', datagrabber, 'deriv_id')
+    wf.connect(deriv_type_infosource, 'deriv_type', datagrabber, 'deriv_type')
+    wf.connect(method_id_infosource, 'method_id', datagrabber, 'method_id')  
     wf.connect(preproc_id_infosource, 'preproc_id', datagrabber, 'preproc_id')
+    wf.connect(pproc_id_infosource, 'pproc_id', datagrabber, 'pproc_id')
     
     #OUTPUT PATHS & LABELS
-    toText = pe.JoinNode(Text_out(), joinsource='subject_id_infosource', joinfield="in_file", name="text_files")
+    toText = pe.JoinNode(Text_out(), joinsource='subject_id_infosource', joinfield="in_file", name="methods_text_files")
+    wf.connect(datagrabber, 'methods_files', toText, 'in_file')
     
-    wf.connect(datagrabber, 'deriv_files', toText, 'in_file')
+    toText2 = pe.JoinNode(Text_out(), joinsource='subject_id_infosource', joinfield="in_file", name="eigen_cent_b_text_files")
+    wf.connect(datagrabber, 'centrality_files', toText2, 'in_file')
          
     return wf
     
--- variables.py
+++ variables.py
@@ -6,23 +6,28 @@ dataset = 'ABIDE'
 
 workingdir = '/home/rschadmin/Data/'+dataset+'working_dir'
 datadir = '/home/rschadmin/Data/'+dataset
+derivdir = datadir+'_DERIV/'
 
 phenodir = '/home/rschadmin/Data/DOCs/'
 phenofiles = os.listdir(phenodir)
 motionfiles = glob.glob(os.path.join(datadir+'/*/power_params/_scan_rest_1_rest/_threshold_0.2/*'))
 
-
 subjects = [s.split('_')[0] for s in os.listdir(datadir)]
+subjects_eign = [s.split('_')[0] for s in os.listdir(os.path.join(derivdir,'Eigen'))]
+subjects_cent = [s.split('_')[0] for s in os.listdir(os.path.join(derivdir,'Cent'))]
 #exclude_subjects = ['2570769'] ADHD
 exclude_subjects = ['0050058'] #ABIDE
-subjects = list(set(subjects) - set(exclude_subjects))
-
-derivs = ['alff_Z_to_standard_smooth','falff_Z_to_standard_smooth']
+subjects = list(set(subjects)&set(subjects_eign)&set(subjects_cent) - set(exclude_subjects))
+methods = ['alff_Z_to_standard_smooth','falff_Z_to_standard_smooth']
+derivs = ['Cent','Eigen']
+deriv_types = ['binarize','weighted']
 #ADHD preprocs =['_compcor_ncomponents_5_selector_pc10.linear1.wm0.global0.motion1.quadratic0.gm0.compcor1.csf0',
 # '_compcor_ncomponents_5_selector_pc10.linear1.wm1.global1.motion1.quadratic0.gm0.compcor0.csf1']
 #ABIDE
 preprocs = ['_compcor_ncomponents_5_selector_pc10.linear1.wm0.global0.motion1.quadratic1.gm0.compcor1.csf0',
 '_compcor_ncomponents_5_selector_pc10.linear1.wm0.global1.motion1.quadratic1.gm0.compcor1.csf0']
+
+pprocs = ['_scan_nofilt_global','_scan_nofilt_noglobal','_scan_filt_global','_scan_filt_noglobal']
 
 #phenotype data
 #ADHD = 'ScanDir ID'
