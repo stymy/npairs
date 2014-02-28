fb65ccde4fd828362612623d14d2d04c62599990
stymy <rschadmin@cmi-rsch-li002.childmind.org>
2014-02-28 14:04:25 -0500

    changed for 3 measures

--- SVC_pipeline.py
+++ SVC_pipeline.py
@@ -14,56 +14,44 @@ from text_out import Text_out
 from classify import Classify
 import numpy as np
 
-from variables import workingdir, datadir, derivdir, outputdir, subjects, scans, preprocs, methods
+from variables import workingdir, datadir, derivdir, outputdir, subjects, scans, preprocs
 
 def get_wf():
     wf = pe.Workflow(name="svc_workflow")
-    wf.base_dir = os.path.join(workingdir,"npairs")
+    wf.base_dir = os.path.join(workingdir,"npairs_3deriv")
     wf.config['execution']['crashdump_dir'] = wf.base_dir + "/crash_files"
 
     #INFOSOURCE ITERABLES
     subject_id_infosource = pe.Node(util.IdentityInterface(fields=['subject_id']), name="subject_id_infosource")
     subject_id_infosource.iterables = [('subject_id', subjects)]
-
-    method_id_infosource = pe.Node(util.IdentityInterface(fields=['method_id']), name="method_id_infosource")
-    method_id_infosource.iterables = ('method_id', methods)
-    
-    #deriv_id_infosource = pe.Node(util.IdentityInterface(fields=['deriv_id']), name="deriv_id_infosource")
-    #deriv_id_infosource.iterables = ('deriv_id', derivs)
     
     scan_id_infosource = pe.Node(util.IdentityInterface(fields=['scan_id']), name= 'scan_id_infosource')
     scan_id_infosource.iterables = ('scan_id', scans)
 
     preproc_id_infosource = pe.Node(util.IdentityInterface(fields=['preproc_id']), name="preproc_id_infosource")
     preproc_id_infosource.iterables = ('preproc_id', preprocs)
-    
-    #pproc_id_infosource = pe.Node(util.IdentityInterface(fields=['pproc_id']), name="pproc_id_infosource")
-    #pproc_id_infosource.iterables = ('pproc_id', pprocs)
-    
-    #deriv_type_infosource = pe.Node(util.IdentityInterface(fields=['deriv_type']), name="deriv_type_infosource")
-    #deriv_type_infosource.iterables = ('deriv_type', deriv_types)
 
     #DATAGRABBER
-    datagrabber = pe.Node(nio.DataGrabber(infields=['subject_id', 'method_id','scan_id','preproc_id'], outfields=['methods_files','mask_file']), name='datagrabber')
+    datagrabber = pe.Node(nio.DataGrabber(infields=['subject_id', 'scan_id','preproc_id'], outfields=['falff_files','dr_files','reho_files','mask_file']), name='datagrabber')
     datagrabber.inputs.base_directory = '/'
     datagrabber.inputs.template = '*'
-    datagrabber.inputs.field_template = dict(methods_files=os.path.join(datadir,'*%s*/%s/_scan_rest_%s_rest/*/*/*/%s/*/*/*/*.nii.gz'),
+    datagrabber.inputs.field_template = dict(dr_files= os.path.join(datadir,'*%s*/dr_tempreg_maps_z_stack_to_standard/_scan_rest_%s_rest/*/*/*/%s/*/*.nii.gz'),
+                                            reho_files= os.path.join(datadir,'*%s*/reho_Z_to_standard_smooth/_scan_rest_%s_rest/*/*/*/%s/*/*.nii.gz'),
+                                            falff_files=os.path.join(datadir,'*%s*/falff_Z_to_standard_smooth/_scan_rest_%s_rest/*/*/*/%s/*/*/*/*.nii.gz'),
                                             mask_file = os.path.join(datadir,'*%s*/functional_brain_mask_to_standard/_scan_rest_%s_rest/*.nii.gz'))
                                             #centrality_files= os.path.join(derivdir,'%s/%s*/*zscore/*/%s/*%s*.nii.gz'),
                                             
-    datagrabber.inputs.template_args = dict(methods_files= [['subject_id', 'method_id','scan_id','preproc_id']],
-                                             mask_file= [['subject_id','scan_id']])
+    datagrabber.inputs.template_args = dict(falff_files= [['subject_id', 'scan_id','preproc_id']],
+                                            reho_files= [['subject_id', 'scan_id','preproc_id']],
+                                            dr_files= [['subject_id', 'scan_id','preproc_id']],
+                                            mask_file= [['subject_id','scan_id']])
                                              #centrality_files= [['deriv_id','subject_id','pproc_id','deriv_type']],
                                            
     datagrabber.inputs.sort_filelist = True
 
     wf.connect(subject_id_infosource, 'subject_id', datagrabber, 'subject_id')
     wf.connect(scan_id_infosource, 'scan_id', datagrabber, 'scan_id')
-    #wf.connect(deriv_id_infosource, 'deriv_id', datagrabber, 'deriv_id')
-    #wf.connect(deriv_type_infosource, 'deriv_type', datagrabber, 'deriv_type')
-    wf.connect(method_id_infosource, 'method_id', datagrabber, 'method_id')  
     wf.connect(preproc_id_infosource, 'preproc_id', datagrabber, 'preproc_id')
-    #wf.connect(pproc_id_infosource, 'pproc_id', datagrabber, 'pproc_id')
     
     #CONCATENATE MASKS
     maskcat = pe.JoinNode(afni.TCat(), joinsource='subject_id_infosource', joinfield='in_files', name='maskcat')
@@ -85,34 +73,42 @@ def get_wf():
     wf.connect(maskmerge, 'out_file', maskthresh, 'in_file_a')
     
     #OUTPUT PATHS & LABELS
-    toText = pe.JoinNode(Text_out(), joinsource='subject_id_infosource', joinfield="in_file", name="methods_text_files")
-    wf.connect(datagrabber, 'methods_files', toText, 'in_file')
+    toText = pe.JoinNode(Text_out(), joinsource='subject_id_infosource', joinfield="in_file", name="falff_text_files")
+    wf.connect(datagrabber, 'falff_files', toText, 'in_file')
 
     
-    #toText2 = pe.JoinNode(Text_out(), joinsource='subject_id_infosource', joinfield="in_file", name="eigen_cent_b_text_files")
-    #wf.connect(datagrabber, 'centrality_files', toText2, 'in_file')
+    toText2 = pe.JoinNode(Text_out(), joinsource='subject_id_infosource', joinfield="in_file", name="reho_text_files")
+    wf.connect(datagrabber, 'reho_files', toText2, 'in_file')
     
-
+    toText3 = pe.JoinNode(Text_out(), joinsource='subject_id_infosource', joinfield="in_file", name="dr_text_files")
+    wf.connect(datagrabber, 'dr_files', toText3, 'in_file')
     
     #RUN CLASSIFIERs
-    classifier = pe.Node(Classify(), name='Classified')
+    classifier = pe.Node(Classify(), name='SVC_falff')
     wf.connect(toText, 'label_file', classifier, 'label_file')
     wf.connect(toText, 'data_paths', classifier, 'path_file')
     wf.connect(maskthresh, 'out_file', classifier, 'mask_file')
     
-    #classifier2 = pe.Node(Classify(), name='SVC_eign_cent')
-    #wf.connect(datagrabber, 'mask_file', classifier2, 'mask_file')
-    #wf.connect(toText2, 'label_file', classifier2, 'label_file')
-    #wf.connect(toText2, 'data_paths', classifier2, 'path_file') 
+    classifier2 = pe.Node(Classify(), name='SVC_reho')
+    wf.connect(toText2, 'label_file', classifier2, 'label_file')
+    wf.connect(toText2, 'data_paths', classifier2, 'path_file') 
+    wf.connect(maskthresh, 'out_file', classifier2, 'mask_file')
+    
+    classifier3 = pe.Node(Classify(), name='SVC_dr')
+    wf.connect(toText3, 'label_file', classifier3, 'label_file')
+    wf.connect(toText3, 'data_paths', classifier3, 'path_file')
+    wf.connect(maskthresh, 'out_file', classifier3, 'mask_file')
     
     #DATASINK
     ds = pe.Node(nio.DataSink(), name='datasink')
     ds.inputs.base_directory = outputdir
     
-    wf.connect(classifier, 'pred', ds, 'prediction_accuracy')
-    wf.connect(classifier, 'rep', ds, 'reproducibility')
-    #wf.connect(classifier2, 'pred', ds, 'prediction_accuracy_cent')
-    #wf.connect(classifier2, 'rep', ds, 'reproducibility_cent')
+    wf.connect(classifier, 'pred', ds, 'prediction_accuracy_falff')
+    wf.connect(classifier, 'rep', ds, 'reproducibility_falff')
+    wf.connect(classifier2, 'pred', ds, 'prediction_accuracy_reho')
+    wf.connect(classifier2, 'rep', ds, 'reproducibility_reho')
+    wf.connect(classifier3, 'pred', ds, 'prediction_accuracy_dr')
+    wf.connect(classifier3, 'rep', ds, 'reproducibility_dr')   
     
     wf.config['execution'] = {
                                'plugin': 'Linear',
--- variables.py
+++ variables.py
@@ -6,16 +6,16 @@ from collections import defaultdict
 dataset = 'ABIDE'
 
 workingdir = '/home/rschadmin/Data/'+dataset+'working_dir'
-#datadir = '/home/rschadmin/Data/'+dataset
-datadir = '/home2/data/Originals/'
+datadir = '/home/rschadmin/Data/'+dataset
 derivdir = datadir+'_DERIV/'
 outputdir = '/home/rschadmin/Data/ABIDE_SVC_output/'
 
-#phenodir = '/home/rschadmin/Data/DOCs/'
-phenodir = '/home2/data/Originals/ABIDE/Docs/Phenotypics_motion_scannerProtocols/PhenotypicDataForConsortiumUse/'
+phenodir = '/home/rschadmin/Data/DOCs/'
+#/home2/data/Originals/ABIDE/Docs/Phenotypics_motion_scannerProtocols/PhenotypicDataForConsortiumUse
 phenofiles = os.listdir(phenodir)
 motiondir = datadir
 #motiondir = '/home2/data/Projects/ABIDE_Initiative/CPAC/Output_2013-11-22/pipeline_MerrittIsland'
+
 motionfiles = glob.glob(os.path.join(motiondir+'/*/power_params/_scan_rest_1_rest/_threshold_0.2/*'))
 
 subjects = set()
@@ -27,9 +27,7 @@ for s in glob.glob(os.path.join(datadir,'*/falff_Z_to_standard_smooth/_scan_rest
 #exclude_subjects = ['2570769'] ADHD
 exclude_subjects = [] #ABIDE
 subjects = list(set(subjects) - set(exclude_subjects))
-methods = ['falff_Z_to_standard_smooth']
 derivs = ['Cent','Eigen']
-deriv_types = ['binarize','weighted']
 scans = ['1']
 #ADHD preprocs =['_compcor_ncomponents_5_selector_pc10.linear1.wm0.global0.motion1.quadratic0.gm0.compcor1.csf0',
 # '_compcor_ncomponents_5_selector_pc10.linear1.wm1.global1.motion1.quadratic0.gm0.compcor0.csf1']
@@ -56,7 +54,6 @@ for i, phenofile in enumerate(phenofiles):
             pheno_dict[row[0]].append(str(i)) #add csv id number
             pheno_dict[row[0]].append(phenofile) # add site name by csv
             phenotyped_subs.add(row[0])
-
 for motionfile in motionfiles:
     with open(motionfile,'rU') as f:
         reader = csv.reader(f,delimiter=",")
